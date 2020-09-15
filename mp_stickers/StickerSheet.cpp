2fca977c971caa2c3f1657c492395f47f08e23d7
Moriah Gau <mgau2@linux-a2.ews.illinois.edu>
2020-09-15 00:30:41 -0500

    updated StickerSheet

--- mp_stickers/StickerSheet.cpp
+++ mp_stickers/StickerSheet.cpp
@@ -1,1 +1,28 @@
 #include "StickerSheet.h"
+using namespace cs225;
+
+
+StickerSheet::StickerSheet(const Image &picture, unsigned max){};
+StickerSheet::~StickerSheet(){};
+StickerSheet::StickerSheet (const StickerSheet &other){};
+const StickerSheet & StickerSheet::operator=(const StickerSheet &other){
+    return *this;
+};
+void StickerSheet::changeMaxStickers(unsigned max){};
+int StickerSheet::addSticker(Image &sticker, unsigned x, unsigned y){
+    int j = 0;
+    return j;
+};
+bool StickerSheet::translate(unsigned index, unsigned x, unsigned y){
+    return 0;
+};
+void StickerSheet::removeSticker(unsigned index){};
+Image* StickerSheet::getSticker(unsigned index){
+    Image *image = new Image();
+    return image;
+
+};
+Image StickerSheet::render()const{
+    Image *image = new Image();
+    return *image;
+};
