23ce8b87e702845b325f839300b6f2ce5bdbd1d8
Moriah Gau <mgau2@linux-a2.ews.illinois.edu>
2020-11-07 01:40:41 -0600

    added skeleton for SquareMaze

--- mp_mazes/maze.cpp
+++ mp_mazes/maze.cpp
@@ -1,1 +1,27 @@
 /* Your code here! */
+#include "maze.h"
+#include "dsets.h"
+
+SquareMaze::SquareMaze(){
+}
+void SquareMaze::makeMaze(int width,int height){
+    return;
+}
+bool SquareMaze::canTravel(int x, int y, int dir) const{
+    return true;
+}
+void SquareMaze::setWall(int x, int y, int dir, bool exists){
+    return;
+}
+vector <int> SquareMaze::solveMaze(){
+    vector<int> temp;
+    return temp;
+}
+cs225::PNG * SquareMaze::drawMaze()const{
+    cs225::PNG * temp = NULL; 
+    return temp;
+}
+cs225::PNG * SquareMaze::drawMazeWithSolution(){
+    cs225::PNG * temp = NULL; 
+    return temp;
+};
--- mp_mazes/maze.h
+++ mp_mazes/maze.h
@@ -1,1 +1,18 @@
 /* Your code here! */
+#pragma once
+#include "cs225/PNG.h"
+#include "dsets.h"
+#include <vector>
+
+class SquareMaze
+{
+  public:
+    SquareMaze();
+    void makeMaze(int width,int height);
+    bool canTravel(int x, int y, int dir) const;
+    void setWall(int x, int y, int dir, bool exists);
+    vector<int> solveMaze();
+    cs225::PNG * drawMaze()const;
+    cs225::PNG * drawMazeWithSolution();
+
+};
